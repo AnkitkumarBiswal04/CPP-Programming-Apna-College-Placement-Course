84ccccb32adcf7b993d5300fafd3ee0f0d768429
Ankit Kumar Biswal <116702754+AnkitkumarBiswal04@users.noreply.github.com>
2024-05-26 13:29:39 +0530

    Create Printnonnegativenumber.cpp

---
+++ C++Programming /Printnonnegativenumber.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+//using while loop printing the number till it not negative
+int main(){
+int n;
+cin>>n;
+
+    while(n>0) {
+        cout << n << endl;
+        cin>>n;
+    }
+return 0;
+}

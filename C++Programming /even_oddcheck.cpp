fc55a835383c615c45ce77227fc24ebf0341894a
Ankit Kumar Biswal <116702754+AnkitkumarBiswal04@users.noreply.github.com>
2024-05-26 12:53:14 +0530

    Create even_oddcheck.cpp

---
+++ C++Programming /even_oddcheck.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+//this code is to check whwthwe it is even or odd
+int main(){
+    int n;
+    cin>>n;
+    if(n%2==0){
+        cout<<"even"<<endl;
+    }
+    else{
+        cout<<"odd"<<endl;
+    }
+    return 0;
+}

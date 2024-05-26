ec6ec3efded7e83a7ea956e9f11735299f8e0ea4
Ankit Kumar Biswal <116702754+AnkitkumarBiswal04@users.noreply.github.com>
2024-05-26 10:58:57 +0530

    Create Datatypes.cpp

---
+++ C++Programming /Datatypes.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+// in this we will get to know thw size of each primitive datatypes 
+int main(){
+    int a;
+    a=12;
+    cout<<"the size of int is "<<sizeof(a)<<endl;
+    float b;
+    cout<<"the size of float is "<<sizeof(b)<<endl;
+    char c;
+    cout<<"the size of char is "<<sizeof(c)<<endl;
+    bool d;
+    cout<<"the size of bool is "<<sizeof(d)<<endl;
+    return 0;
+}

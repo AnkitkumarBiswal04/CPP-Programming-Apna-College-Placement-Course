3705b5b5bc18c26d2a2c07dd8e3b25500f90763d
Ankit Kumar Biswal <116702754+AnkitkumarBiswal04@users.noreply.github.com>
2024-05-26 11:41:35 +0530

    Create inputandoutput.cpp

---
+++ C++Programming /inputandoutput.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+//this code is to understand how to take input and output in C++ program
+int main(){
+    //taking input
+    int amount1;
+    cin>>amount1;
+    int amount2;
+    cin>>amount2;
+    //taking output 
+    int sum=amount1+amount2;
+    cout<<"sum of amount1 and amount2 is:"<<sum<<endl;
+    return 0;
+}
+  

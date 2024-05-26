50dbbff43b6c9ed39d77700f8472b2196f88cba4
Ankit Kumar Biswal <116702754+AnkitkumarBiswal04@users.noreply.github.com>
2024-05-26 12:08:27 +0530

    Create ifelsestatement.cpp

---
+++ C++Programming /ifelse.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+// this code is to understand if else statement
+int main(){
+    int savings;
+    cin>>savings;
+    if(savings>50){
+        cout<<"pizza"<<endl;
+    }
+    else{
+        cout<<"Burger"<<endl;
+    }
+    return 0;
+    
+}
